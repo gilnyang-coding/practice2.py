f8036f0ac2a033ceb4a7a6805709da18901ce82e
gilnyang-coding <kdw061224@gmail.com>
2024-12-14 19:47:00 +0900

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,14 @@
+#자료 형태 바꾸기기
+menu = {"우유", "커피", "주스"}
+print(menu,type(menu))
+
+menu=list(menu)
+print(menu,type(menu))
+
+menu=tuple(menu)
+print(menu,type(menu))
+
+menu=set(menu)
+print(menu,type(menu))
+
+#if
